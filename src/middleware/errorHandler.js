f249ef0a11f07fb8c9bc4376b8ef6d587d70b95b
Dmode <dmode90@gmail.com>
2025-10-07 15:16:09 +0300

    error handler refine

--- src/middleware/errorHandler.js
+++ src/middleware/errorHandler.js
@@ -2,14 +2,13 @@ import { HttpError } from 'http-errors';
 
 // eslint-disable-next-line no-unused-vars
 export const errorHandler = (err, req, res, next) => {
-  console.error('Error:', err.message);
-
   // Якщо помилка створена через http-errors
   if (err instanceof HttpError) {
     return res.status(err.status).json({
-      error: err.message || err.name,
+      message: err.message || err.name,
     });
   }
+
   const isProd = process.env.NODE_ENV === 'production';
 
   // Усі інші помилки — як внутрішні
--- src/models/note.js
+++ src/models/note.js
@@ -10,12 +10,11 @@ const noteSchema = new Schema(
     },
     content: {
       type: String,
-      required: false,
       trim: true,
+      default: '',
     },
     tag: {
       type: String,
-      required: true,
       enum: [
         'Work',
         'Personal',
