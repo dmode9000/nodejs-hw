8dcf60f73eccb6a77dd4692749e04069f7965626
Dmode <dmode90@gmail.com>
2025-10-14 00:48:49 +0300

    enum from constant

--- src/models/note.js
+++ src/models/note.js
@@ -1,5 +1,6 @@
 import { Schema } from 'mongoose';
 import { model } from 'mongoose';
+import { TAGS } from '../constants/tags.js';
 
 const noteSchema = new Schema(
   {
@@ -15,19 +16,8 @@ const noteSchema = new Schema(
     },
     tag: {
       type: String,
-      enum: [
-        'Work',
-        'Personal',
-        'Meeting',
-        'Shopping',
-        'Ideas',
-        'Travel',
-        'Finance',
-        'Health',
-        'Important',
-        'Todo',
-      ],
-      default: 'Todo',
+      enum: TAGS,
+      default: TAGS.at(-1),
     },
   },
   {
